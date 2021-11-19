fc92f64ac15d868f10f5c5e48f0e527689e81461
GodfredAsa <goldenheroe1@yahoo.com>
2021-11-19 18:57:07 +0000

    where we got to need finalizing

--- src/com/company/BlackJack.java
+++ src/com/company/BlackJack.java
@@ -22,43 +22,77 @@ public class BlackJack {
                 new Player("Cici", deck.sendCards())
         );
 
-        for(Player p: playerList){
-            if(p.getHandTotal()<17){
-                System.out.println(p.getName() + " --> " + p.getHandTotal());
-                p.hit(deck.singleCard());
-
-                System.out.println(p.getName() + " --> " + p.getHandTotal());
+//        for(Player p: playerList){
+//            if(p.getHandTotal()<17){
+//                System.out.println(p.getName() + " --> " + p.getHandTotal());
+//                p.hit(deck.singleCard());
+//                System.out.println(p.getName() + " --> " + p.getHandTotal());
+////
+//            }else if(p.getHandTotal()>21){
+//                System.out.println(p.getName() + "--> " + p.getHandTotal() + " Bustered");
+//               p.goBust();
 //
-            }else if(p.getHandTotal()>21){
-                System.out.println(p.getName() + "--> " + p.getHandTotal());
-               p.goBust();
-            }else if(p.getHandTotal()>17 && p.getHandTotal()<21){
-                System.out.println(p.getName() + "--> " + p.getHandTotal() + "  Winner");
-//                p.getName();
+//            }else if(p.getHandTotal()>17 && p.getHandTotal()<21){
+//                System.out.println(p.getName() + "--> " + p.getHandTotal() + "  Winner");
+//            }
+//        }
+
+
+        System.out.println("******************   -------------------   ******************");
+
+      for(int i=0; i<2; i++){
+          for(Player player: playerList){
+
+              if(player.getHandTotal()<17){
+//                System.out.println(player.getName() + " --> " + player.getHandTotal());
+                player.hit(deck.singleCard());
+
+                  System.out.println(player.getName() + " -- [hit]--> " + player.getHandTotal());
+                if(player.getHandTotal()<17){
+                    System.out.println(player.getName() + "--> " + player.getHandTotal() + " lose");
+                }else if(player.getHandTotal()>21){
+                    System.out.println(player.getName() + "--> " + player.getHandTotal() + " lose");
+                }else if(player.getHandTotal()>17 && player.getHandTotal()<21){
+                    System.out.println(player.getName() + "--> " + player.getHandTotal() + "  Winner");
+                }
+
+
+            }else if(player.getHandTotal()>21){
+                System.out.println(player.getName() + "--> " + player.getHandTotal() + " lose");
+
+            }else if(player.getHandTotal()>17 && player.getHandTotal()<21){
+                System.out.println(player.getName() + "--> " + player.getHandTotal() + "  Winner");
             }
 
-//            System.out.println(p.getName());
-        }
+          }
+      }
 
-        System.out.println(playerList.get(0));
-
-
-        System.out.println("------------------******----------------");
-        playerList.stream().map(s->s.getName() +"->"+ s.getHandTotal()).forEach(System.out::println);
-        System.out.println("---------------******---------------------");
-
-        playerList.stream().map(s->s.getName() + s.getHandTotal()).forEach(System.out::println);
+        System.out.println("******************   -------------------   ******************");
 
 
-        System.out.println("---------------******---------------------");
 
-        Player p1 = new Player("A", deck.sendCards());
+//        System.out.println(playerList.get(0));
+//
+//
+//        System.out.println("------------------******----------------");
+//        playerList.stream().map(s->s.getName() +"->"+ s.getHandTotal()).forEach(System.out::println);
+//        System.out.println("---------------******---------------------");
+//
+//        playerList.stream().map(s->s.getName() + s.getHandTotal()).forEach(System.out::println);
+
+
+//        System.out.println("---------------******---------------------");
+
+//        Player p1 = new Player("A", deck.sendCards());
 //        System.out.println("Dealt Hand Cards: "+ p1.getDealtHand());
 //
 ////        p1.hit(deck.singleCard());
 //        System.out.println("----------------------------------");
 //        System.out.println( p1.hit(deck.singleCard()));
 //        System.out.println("------------------------------------");
+
+        /**
+         *
 
         if(p1.totalPlayerCardPoints()<17){
 
@@ -79,7 +113,7 @@ public class BlackJack {
 
        p1.goBust();
 
-
+         */
 
     }
 
